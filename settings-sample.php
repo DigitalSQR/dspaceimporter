7258e56e1c39c4316f54d42dbf533a9615e96862
Sovello Hildebrand <sovellohpmgani@gmail.com>
2020-06-17 09:46:43 -0400

    add settings template file.

---
+++ settings-sample.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_USER', 'root');
+define('DB_PASSWD', 'root');
+define('DB_PORT', 3306);
+define('DB_HOST', 'localhost');
+define('FURL', '/var/lib/gdhn/');
+define('COLLECTION', 'gdhn');
